192531292a1b306ee6439284d333835b734a739d
ivangfdias <ivangfdias@gmail.com>
2024-10-23 10:29:27 -0300

    Isolated method grammar, implemented connection tune

--- Connection.c
+++ Connection.c
@@ -45,7 +45,7 @@ void overwrite_rule_set_length(char *rule_to_decode, char *length_rule,
 
   int decoded_rule_length = 0;
   unsigned char *decoded_rule =
-      decode_rule(rule_to_decode, &decoded_rule_length, connection_grammar);
+      decode_rule(rule_to_decode, &decoded_rule_length, grammar);
 
   grammar_insert(grammar, rule_to_decode,
                  new_grammar_entry_t(BYTE_ARRAY, (char *)decoded_rule, NULL,
@@ -61,40 +61,56 @@ void overwrite_rule_set_length(char *rule_to_decode, char *length_rule,
 }
 
 unsigned char *connection_start_ok(int *sent_packet_size) {
-
-  /* SETTING UP CONNECTION GRAMMAR */
-
-  grammar_insert(connection_grammar, "short-string",
-                 new_function_grammar_entry(shortstring_generator));
-
-  grammar_insert(connection_grammar, "long-string",
-                 new_function_grammar_entry(longstring_generator));
+  Grammar mutable_grammar = copy_grammar(connection_grammar);
 
   /* SETTING UP METHOD MESSAGE */
-  grammar_insert(connection_grammar, "method-id",
+  grammar_insert(mutable_grammar, "method-id",
                  new_string_grammar_entry("m11-method-id"));
 
-  grammar_insert(connection_grammar, "method-properties",
+  grammar_insert(mutable_grammar, "method-properties",
                  new_string_grammar_entry("m11-method-properties"));
 
   /* GOING THROUGH METHOD SPECIFIC STUFF */
   overwrite_rule_set_length("client-properties-payload",
-                            "client-properties-length", 4, connection_grammar);
+                            "client-properties-length", 4, mutable_grammar);
 
   /* PLAIN AUTHENTICATION METHOD */
-  grammar_insert(connection_grammar, "authcid",
+  grammar_insert(mutable_grammar, "authcid",
                  new_string_grammar_entry("\"username\""));
 
-  grammar_insert(connection_grammar, "passwd",
+  grammar_insert(mutable_grammar, "passwd",
                  new_string_grammar_entry("\"password\""));
 
-  overwrite_rule_set_length("message", "message-length", 4, connection_grammar);
+  overwrite_rule_set_length("message", "message-length", 4, mutable_grammar);
+
+  /* PREPARING METHOD PACKET */
+
+  overwrite_rule_set_length("method-payload", "payload-size", 4,
+                            mutable_grammar);
+  unsigned char *result =
+      decode_rule("method", sent_packet_size, mutable_grammar);
+
+  return result;
+}
+
+unsigned char *connection_tune_ok(int *sent_packet_size) {
+
+  Grammar mutable_grammar = copy_grammar(connection_grammar);
+
+  /* SETTING UP METHOD MESSAGE */
+  grammar_insert(mutable_grammar, "method-id",
+                 new_string_grammar_entry("m31-method-id"));
+
+  grammar_insert(mutable_grammar, "method-properties",
+                 new_string_grammar_entry("m31-method-properties"));
 
   /* PREPARING METHOD PACKET */
   overwrite_rule_set_length("method-payload", "payload-size", 4,
-                            connection_grammar);
+                            mutable_grammar);
+  unsigned char *result =
+      decode_rule("method", sent_packet_size, mutable_grammar);
 
-  return decode_rule("method", sent_packet_size, connection_grammar);
+  return result;
 }
 
 unsigned char *connection_packet_decider(method_struct *method_data,
@@ -106,15 +122,26 @@ unsigned char *connection_packet_decider(method_struct *method_data,
     *next_state = 0;
     return NULL;
   }
-  if (connection_grammar == NULL)
-    connection_grammar = generate_grammar("../grammar/grammar-connection");
+  if (connection_grammar == NULL) {
+    /* SETTING UP CONNECTION GRAMMAR */
+    connection_grammar = generate_grammar("./grammars/grammar-connection");
 
+    grammar_insert(connection_grammar, "short-string",
+                   new_function_grammar_entry(shortstring_generator));
+
+    grammar_insert(connection_grammar, "long-string",
+                   new_function_grammar_entry(longstring_generator));
+  }
+
+  printf("Received method_id = %u\n", method_data->method_id);
   switch (method_data->method_id) {
   case START:
     *next_state = 1;
     return connection_start_ok(sent_packet_size);
     break;
   case TUNE:
+    *next_state = 2;
+    return connection_tune_ok(sent_packet_size);
   case SECURE:
   case OPEN:
   case CLOSE:
